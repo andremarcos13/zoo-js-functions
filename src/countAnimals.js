e850c7d2fcb9ce66c432219b522a0dc7a246156b
Andre Cesar <andrmgc@gmail.com>
2022-01-27 15:56:28 -0300

    Requisito 2 - tentando com find, ainda nao deu certo

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,21 @@
+const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
-function countAnimals(animal) {
-  // seu código aqui
-}
+// Implemente a função getAnimalsOlderThan
+// Esta função, a partir do nome de uma espécie e uma idade mínima,
+// verifica se todos os animais daquela espécie possuem a idade mínima especificada.
 
+// Observações técnicas
+
+// Deve retornar um valor booleano.
+// O que será avaliado
+
+// Ao passar o nome de uma espécie e uma idade, testa se todos os animais desta espécie possuem a idade mínima especificada.
+
+function countAnimals(...animal) {
+  // seu código aqui
+  // return species.filter((element) => animal.includes(element.name));
+  return species.find(() => animal.popularity);
+}
+console.log();
 module.exports = countAnimals;
--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -1,8 +1,25 @@
+const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
+
+// Implemente a função getAnimalsOlderThan
+// Esta função, a partir do nome de uma espécie e uma idade mínima,
+// verifica se todos os animais daquela espécie possuem a idade mínima especificada.
+
+// Observações técnicas
+
+// Deve retornar um valor booleano.
+// O que será avaliado
+
+// Ao passar o nome de uma espécie e uma idade, testa se todos os animais desta espécie possuem a idade mínima especificada.
 
 function getAnimalsOlderThan(animal, age) {
   // seu código aqui
-  return animal;
+  // if (species.find((element) => element.name === animal) && species.filter((element) => element.residents.age >= age)) {
+  //   return true;
+  // }
+  const socorroJesus = species.find((element) => element.name === animal);
+  // const socorroDeus = species.filter((element) => element.residents.age >= age);
+  // return socorroDeus;
+  return socorroJesus;
 }
-
 module.exports = getAnimalsOlderThan;
