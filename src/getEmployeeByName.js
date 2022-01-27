5e64198507be7b50d60bb67288947df4f5e5ebde
Andre Cesar <andrmgc@gmail.com>
2022-01-27 18:02:45 -0300

    Requisito 3 - utilizando find e if - com ajuda do colega Heitor

--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -1,7 +1,22 @@
+const { employees } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
+
+// Implemente a função getEmployeeByName
+// Esta função é responsável pela busca das pessoas colaboradoras através do primeiro ou do último nome delas
+
+// O que será avaliado
+
+// Sem parâmetros, retorna um objeto vazio
+// Quando provido o primeiro nome do funcionário, retorna o objeto do funcionário
+// Quando provido o último nome do funcionário, retorna o objeto do funcionário
 
 function getEmployeeByName(employeeName) {
   // seu código aqui
+  if (employeeName === undefined) {
+    return {};
+  }
+  // const getName = employees.find((element) => element.firstName === employeeName || element.lastName === employeeName);
+  return employees.find((element) => element.firstName === employeeName || element.lastName === employeeName);
 }
-
+getEmployeeByName();
 module.exports = getEmployeeByName;
