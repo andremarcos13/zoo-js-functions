8a8a031a19d9aeafa1721396d697a56be8906c11
Andre Cesar <andrmgc@gmail.com>
2022-01-30 13:11:27 -0300

    Requisito 5 - utilizando for, some, filter e map

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -16,10 +16,28 @@ function isManager(id) {
       return true;
     }
   }
-  return undefined;
+  return false;
 }
+// getRelatedEmployees - que utiliza a primeira função para apresentar as seguintes saídas:
+// se for uma pessoa colaboradora gerente, deve retornar um array contendo os nomes das pessoas colaboradoras que ela é responsável;
+// se não for uma pessoa colaboradora gerente, deverá ser lançado um erro gerado com a função
+// construtora Error da biblioteca padrão do JavaScript com a mensagem "O id inserido não é de uma pessoa colaboradora gerente!".
+
 function getRelatedEmployees(managerId) {
   // seu código aqui
+  if (isManager(managerId)) {
+    const employeeWithManagers = employees.filter((element) => {
+      const { managers } = element;
+      return managers.some((currentValue) => currentValue === managerId);
+    });
+
+    const getClerk = employeeWithManagers.map((element) => {
+      const { firstName, lastName } = element;
+      return `${firstName} ${lastName}`;
+    });
+    return getClerk;
+  }
+  throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');
 }
 
 module.exports = { isManager, getRelatedEmployees };
